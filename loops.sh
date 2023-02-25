9e5cc2b3c11b78a78cd85900f1897532096b4601
Aravind Leela <aravindleela.a@gmail.com>
2023-02-25 16:08:04 +0530

    case command

--- loops.sh
+++ loops.sh
@@ -2,13 +2,13 @@
 i=20
 while [ $i -gt 0 ]; do
   echo $i
-  i=$(($-1))
+  i=$(($i-1))
   sleep 1
- done
+done
 
 
  ## Loope based on inputs
  for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 ; do
    echo $i
    sleep 1
-  Done
+Done

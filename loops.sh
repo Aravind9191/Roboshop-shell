3e6b1c396efd5c1464bd912b180c7d9737330dce
Aravind Leela <aravindleela.a@gmail.com>
2023-02-25 16:04:51 +0530

    case command

---
+++ loops.sh
@@ -0,0 +1,14 @@
+# Loops based expression
+i=20
+while [ $i -gt o ]; do
+  echo $i
+  i=$(($-1))
+  sleep 1
+ done
+
+
+ ## Loope based on inputs
+ for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 ; do
+   echo $i
+   sleep 1
+ Done
